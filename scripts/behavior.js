356d33d3eb391d31ead0ff1d4296c4e661f1c08b
Maria Ribeiro <mia.francisco.ribeiro@gmail.com>
2023-03-27 21:15:46 +0100

    Donut chart + data

--- scripts/behavior.js
+++ scripts/behavior.js
@@ -1,6 +1,14 @@
 function init() {
         
-	createDonut();
+	
+    createDonut();
+    console.log("adus");
+    d3.csv("/data/airline.csv", function(data) {
+        //console.log(data.airline);
+        //console.log(data.airline_sentiment);
+        //console.log(data.negativereason);
+        //console.log(data.name);
+    });
 }
 
 function createDonut() {
@@ -10,7 +18,7 @@ function createDonut() {
     svg = d3.select("div#donut")
 			.append("svg")
         width = window.innerWidth/2.5, 
-        height = window.innerHeight/2,
+        height = window.innerHeight/1.5,
         innerRadius = Math.min(width, height) / 4
         outerRadius = Math.min(width, height) / 2,
         g = svg.append("g").attr("transform", "translate(" + width / 2.5 + "," + height / 2 + ")");
@@ -40,74 +48,5 @@ function createDonut() {
             return color(i);
         })
         .attr("d", arc);
-
-    /*var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
-    var data = [2, 4, 8, 10]
-    const width = window.innerWidth * 0.42;
-	height = window.innerHeight * 0.383;
-
-
-		const pie = d3.pie()
-			.value(d => d[1])
-
-		const stroke = d3.scaleOrdinal()
-			.range(["#b94366", "#ffe6ee"])
-
-		const fill = d3.scaleOrdinal()
-			.range(["#ff1493", "white"])
-
-		//const data_aux = pie(data)
-
-        svg = d3.select("div#donut")
-			.append("svg")
-        
-        g = svg.append("g")
-
-
-        var arc = d3.arc()
-            .innerRadius(30)
-            .outerRadius(100);
-        
-        var arcs = g.selectAll("arc")
-            .data(pie(data))
-            .enter()
-            .append("g")
-            .attr("class", "arc")
-
-            arcs.append("path")
-            .attr("fill", function(d, i) {
-                return color(i);
-            })
-            .attr("d", arc);*/
-
-        
-		/*svg.append("text")
-			.text("Women")
-			.attr("font-size", "14px")
-			.attr("transform", `translate(${width / 8},${height / 14})`);*/
-
-		/*svg
-			.data(data_aux)
-			.join('path')
-			.attr('d', d3.arc()
-				.innerRadius(width / 12)
-				.outerRadius(width / 7)
-			)*/
-			/*.attr('fill', d => fill(d.data))
-			.style("opacity", 1)
-			.style("stroke", d => stroke(d.data))
-			.style("stroke-width", 2)*/
-
-
-		/*svg.selectAll('path')
-			.attr("transform", `translate(${width / 6.3},${height / 2})`);*/
-
-		/*svg.append("text")
-			.attr("text-anchor", "middle")
-			.transition()
-			.duration(200)
-			.text(value + "%")
-			.attr("font-size", "20px")
-			.attr("transform", `translate(${width / 6.3},${height / 2})`);*/
 
 }

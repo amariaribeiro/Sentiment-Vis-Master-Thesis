02b188319127c8f6a4730fd0b85055965eb28ae8
Maria Ribeiro <mia.francisco.ribeiro@gmail.com>
2023-03-29 17:05:14 +0100

    Refactor

--- donuts/scripts/behavior.js
+++ donuts/scripts/behavior.js
@@ -4,7 +4,10 @@ var count = 0;
 var start = true;
 
 function init() {
-    info = d3.csv("/data/airline.csv").then(function(data) {
+    console.log("bom dia")
+    info = d3.csv("../donuts/data/airline.csv").then(function(data) {
+
+        console.log("ola")
 
         var air_counts = d3.rollup(data, v=> v.length, d => d.airline, d => d.airline_sentiment);
         var totals = d3.rollup(data, v => v.length, d => d.airline);
